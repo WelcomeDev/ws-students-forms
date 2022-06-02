383c656c628cae7d4511b953283d8748d061df78
Fedor Ishchenko <Fedor.Ishchenko18@gmail.com>
2022-06-02 20:55:29 +1000

    fix: editions

--- js/validate.js
+++ js/validate.js
@@ -1,14 +1,24 @@
 const requiredFields = ['phone', 'last-name', 'first-name', 'email'];
-const PHONE_PATTERN = /\+7\(\d{3}\)\d{3}-\d{2}\d{2}/;
-const NAME_PATTERN = /[А-Яа-я]{2,}/;
+const PHONE_PATTERN = /^\+7\(\d{3}\)\d{3}-\d{2}\d{2}$/;
+const NAME_PATTERN = /^[А-Яа-я]{2,}$/;
 const patternsFields = {
-    'last-name': NAME_PATTERN,
-    'first-name': NAME_PATTERN,
-    'phone': PHONE_PATTERN
+    'last-name': {
+        pattern: NAME_PATTERN,
+        errorMessage: 'Second name must include characters only'
+    },
+    'first-name': {
+        pattern: NAME_PATTERN,
+        errorMessage: 'Name must include characters only'
+    },
+    'phone': {
+        pattern: PHONE_PATTERN,
+        errorMessage: 'Invalid phone number'
+    }
 };
 
 window.addEventListener('load', () => {
     const inputs = Array.from(document.querySelectorAll('#applicant-form input'));
+    const VALIDATION_MESSAGE_ATTRIBUTE = 'data-pattern-error';
 
     function setValidation(inputs) {
         inputs.filter(input => requiredFields.includes(input.name))
@@ -16,8 +26,13 @@ window.addEventListener('load', () => {
 
         // не очень очевидная проверка, но если взять поле, которого в объекте нет, то вернется undefined, что даст false
         inputs.filter(input => patternsFields[input.name])
-            // устаналиваем в паттерн свойство source объекта "типа" RegExp, чтобы валидация была адекватная
-            .forEach(input => input.pattern = patternsFields[input.name].source);
+            .forEach(input => {
+                const inputValidation = patternsFields[input.name];
+                // устаналиваем в паттерн свойство source объекта "типа" RegExp, чтобы валидация была адекватная
+                input.pattern = inputValidation.pattern.source;
+                // а вот тут мы нагло устанавливаем атрибут с сообщением об ошибке, потом мы его будем выводить в сообщение
+                input.setAttribute(VALIDATION_MESSAGE_ATTRIBUTE, inputValidation.errorMessage);
+            });
     }
 
     const errors = {
@@ -26,21 +41,41 @@ window.addEventListener('load', () => {
 
     function validate(inputEvent) {
         const input = inputEvent.target;
+        const inputValue = input.value;
+        const inputErorr = errors[input.name];
 
-        if (input.required && !input.value && !errors[input.name]) {
-            const errorElement = document.createElement('p');
-            errorElement.innerText = "This field is required";
+        const isEmpty = input.required && !inputValue;
+        const invalidData = input.pattern && !RegExp(input.pattern).test(inputValue);
+        // "хитрая" проверка валидации - сработает тот case который удовлетворяет условие первым
+        switch (true) {
+            case isEmpty:
+                console.log(input.name, ' is required');
+                createError('This field is required');
+                break;
+            case invalidData:
+                console.log('invalid pattern ', input.name);
+                createError(input.getAttribute(VALIDATION_MESSAGE_ATTRIBUTE));
+                break;
+            default:
+                console.log('on remove error');
+                inputErorr?.remove();
+                errors[input.name] = null;
+                break;
+        }
+
+        function createError(text) {
+            // создаем новый элемент, если нету ошибки
+            const errorElement = errors[input.name] ?? document.createElement('p');
+            // обновляем сообщение
+            errorElement.innerText = text;
+
+            // если элемент уже есть, нам больше ничего не нужно делать, выходим
+            if (errors[input.name]) return;
+
             errorElement.classList.add('input-error');
-
-            input.parentNode.addChildren(p);
-            errors[input.name] = p;
+            input.parentNode.appendChild(errorElement);
+            errors[input.name] = errorElement;
         }
-        else {
-            errors[input.name].remove();
-        }
-        // if (!!input.validity.valid) input.setCustomValidity("")
-        // else if (input.required && !input.value) input.setCustomValidity('REQUIRED FIELD')
-        // else if (input['pattern'] && !input.value.match(input['pattern'])) input.setCustomValidity("INVALID PATTERN")
     }
 
     setValidation(inputs);
