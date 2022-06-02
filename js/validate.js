d25f64139ef04cbe32b67804041b61e0f5839a53
Fedor Ishchenko <Fedor.Ishchenko18@gmail.com>
2022-06-02 20:22:34 +1000

    add: multiple validation

--- js/validate.js
+++ js/validate.js
@@ -1,29 +1,48 @@
-const requiredFields = ['phone', 'last-name', 'first-name', 'email']
+const requiredFields = ['phone', 'last-name', 'first-name', 'email'];
+const PHONE_PATTERN = /\+7\(\d{3}\)\d{3}-\d{2}\d{2}/;
+const NAME_PATTERN = /[А-Яа-я]{2,}/;
+const patternsFields = {
+    'last-name': NAME_PATTERN,
+    'first-name': NAME_PATTERN,
+    'phone': PHONE_PATTERN
+};
 
 window.addEventListener('load', () => {
     const inputs = Array.from(document.querySelectorAll('#applicant-form input'));
 
-    inputs.filter(input => requiredFields.includes(input.name))
-        .forEach(input => input.required = true)
+    function setValidation(inputs) {
+        inputs.filter(input => requiredFields.includes(input.name))
+            .forEach(input => input.required = true);
 
-    const phoneInput = inputs.find(input => input.name === 'phone');
-    phoneInput.pattern = /\d{4}/
+        // не очень очевидная проверка, но если взять поле, которого в объекте нет, то вернется undefined, что даст false
+        inputs.filter(input => patternsFields[input.name])
+            // устаналиваем в паттерн свойство source объекта "типа" RegExp, чтобы валидация была адекватная
+            .forEach(input => input.pattern = patternsFields[input.name].source);
+    }
+
+    const errors = {
+
+    };
 
     function validate(inputEvent) {
-        console.log(inputEvent);
         const input = inputEvent.target;
-        input.setCustomValidity("");
 
-        // if(!!input.validity.valid) todo: remove element
-        if(input.required && !input.value){
-                const textElement = document.createElement('p');
-                textElement.innerText = "This field is required";
-                myimg.parentNode.insertBefore(text, myimg.nextSibling)
+        if (input.required && !input.value && !errors[input.name]) {
+            const errorElement = document.createElement('p');
+            errorElement.innerText = "This field is required";
+            errorElement.classList.add('input-error');
+
+            input.parentNode.addChildren(p);
+            errors[input.name] = p;
+        }
+        else {
+            errors[input.name].remove();
         }
         // if (!!input.validity.valid) input.setCustomValidity("")
         // else if (input.required && !input.value) input.setCustomValidity('REQUIRED FIELD')
         // else if (input['pattern'] && !input.value.match(input['pattern'])) input.setCustomValidity("INVALID PATTERN")
     }
 
+    setValidation(inputs);
     inputs.forEach(input => input.addEventListener('blur', validate));
 });
